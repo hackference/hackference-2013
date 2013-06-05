a8027dba3781b10e41bcb5ca31b94216b05a37bd
Mike Elsmore <mikee@winningmoves.com>
2013-06-05 14:01:12 +0100

    Add ticket buy page

---
+++ buy.php
@@ -0,0 +1,17 @@
+    <div id="sub">
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h2>Buy Tickets</h2>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10">
+                <div class="paras" >
+                    <p>Tickets will be available soon.</p>
+                </div>
+				<!-- EventBrite JS Block -->
+				<!-- EventBrite JS Block -->
+			</div>
+		</div>
+	</div>
