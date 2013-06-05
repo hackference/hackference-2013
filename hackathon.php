e5525a29c8758905beb30d7aacd2fd39fb36f69c
Mike Elsmore <mikee@winningmoves.com>
2013-06-05 13:05:03 +0100

    Add more copy Complete location page Add location banner

--- hackathon.php
+++ hackathon.php
@@ -1,57 +1,34 @@
-	<div id="sub">
+    <div id="sub">
 		<div class="section group">
 			<div class="col span_10_of_10 sub_heading">
-				<h2>Schedule</h2>
+				<h2>Show off what you can build</h2>
 				<span class="line"></span>
 			</div>
 		</div>
-
-		<div class="section group">		
-
-			<div class="col span_10_of_10">
-                <div class="col span_10_of_10 sub_heading">
-                    <h3>Conference - 23rd August</h3>
-                    <span class="line"></span>
-                </div>
-                <div class="col span_10_of_10">
-                    <ul>
-                        <li>
-                            <h4>08:30 - 09:30 Registration</h4>
-                            <p>Doors open for people to come in, grab coffee and register for the day</p>
-                        </li>
-                    </ul>
-                </div>
-			</div>
-
-		</div>
-
 		<div class="section group">
-            
-            <div class="col span_10_of_10 sub_heading">
-                <h3>Hackathon</h3>
-                <span class="line"></span>
-            </div>
-            
-            <div class="col span_10_of_10 sub_heading">
-                
-                <div class="col span_5_of_10 speakerSingle">
-                    
-                    <div class="col span_10_of_10 sub_heading">
-                        <h3>24th August</h3>
-                    </div>
-                    
-                </div>
-
-                <div class="col span_5_of_10 speakerSingle">
-                    
-                    <div class="col span_10_of_10 sub_heading">
-                        <h3>25th August</h3>
-                    </div>
-                    
-                </div>
-                
-            </div>
-			
+			<div class="col span_6_of_10">
+				<div class="paras">
+					<p>After a full day of talks from experts, time to test your metal </p>
+				</div>
+			</div>
+            <div class="col span_4_of_10" >
+                <img src="" />
+            </div> 
 		</div>
-		
+	</div>
+
+	<div id="sub">
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h3>Hackathon</h3>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">			
+			<div class="col span_10_of_10">
+				<div class="paras_small">
+					<p>After the conference a whole weekend of coding is afoot (August 31th and September 1st). That means 100 developers gathering together to build some great applications on different platforms in under 36 hours. With access to the knowledge gained at the conference and the APIs and SDKs of the hackathon providers, who will walk away with the prizes?</p>
+				</div>
+			</div>
+		</div>
 	</div>
--- location.php
+++ location.php
@@ -1,34 +1,80 @@
+    <div id="hero">
+		<div class="section group">
+			<div class="col span_10_of_10">
+				<img src='/img/location_banner.jpg' />
+			</div>
+		</div>
+	</div>
+
     <div id="sub">
 		<div class="section group">
 			<div class="col span_10_of_10 sub_heading">
-				<h2>Show off what you can build</h2>
+				<h2>Best venues in Birmingham</h2>
 				<span class="line"></span>
 			</div>
 		</div>
 		<div class="section group">
-			<div class="col span_6_of_10">
+			<div class="col span_10_of_10">
 				<div class="paras">
-					<p>After a full day of talks from experts, time to test your metal </p>
+					<p>To get the most of Hackference it's being held in Digbeth, one of Birmingham's growing tech areas. Surrounded by like minded people with a flare for the creative you can find bustling cafe's and shops.</p>
 				</div>
 			</div>
-            <div class="col span_4_of_10" >
-                <img src="" />
-            </div> 
 		</div>
 	</div>
 
 	<div id="sub">
 		<div class="section group">
 			<div class="col span_10_of_10 sub_heading">
-				<h3>Hackathon</h3>
+				<h3>Fazeley Studios</h3>
 				<span class="line"></span>
 			</div>
 		</div>
 		<div class="section group">			
-			<div class="col span_10_of_10">
+			<div class="col span_6_of_10">
 				<div class="paras_small">
-					<p>After the conference a whole weekend of coding is afoot (August 31th and September 1st). That means 100 developers gathering together to build some great applications on different platforms in under 36 hours. With access to the knowledge gained at the conference and the APIs and SDKs of the hackathon providers, who will walk away with the prizes?</p>
+					<p>Home to some of the city's best digital and creative agencies, Fazeley Studios is the perfect environment for the conference portion of Hackference.</p>
+                    <p>Fazeley Stuios is within walking distance of both Birmingham New Street and Birmingham Moor Street Rail Stations, as well as many major bus routes and Birmingham Coach Station.</p>
+                    <p class="left-align" >
+                        <strong>Address:</strong>
+                        <br/>
+                        191 Fazeley Street, 
+                        <br/>
+                        Digbeth, Birmingham, 
+                        <br/>
+                        B5 5SE
+                    </p>
 				</div>
+			</div>		
+			<div class="col span_4_of_10">
+                <iframe width="100%" height="250px0" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Fazeley+Studios,+Fazeley+Street,+Birmingham&amp;aq=3&amp;oq=fazele&amp;sll=52.57635,-2.329102&amp;sspn=7.240636,25.686035&amp;ie=UTF8&amp;hq=Fazeley+Studios,&amp;hnear=Fazeley+St,+Birmingham,+United+Kingdom&amp;t=m&amp;ll=52.477749,-1.881709&amp;spn=0.001307,0.002146&amp;z=16&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="https://maps.google.co.uk/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Fazeley+Studios,+Fazeley+Street,+Birmingham&amp;aq=3&amp;oq=fazele&amp;sll=52.57635,-2.329102&amp;sspn=7.240636,25.686035&amp;ie=UTF8&amp;hq=Fazeley+Studios,&amp;hnear=Fazeley+St,+Birmingham,+United+Kingdom&amp;t=m&amp;ll=52.477749,-1.881709&amp;spn=0.001307,0.002146&amp;z=16&amp;iwloc=A" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+			</div>
+		</div>
+	</div>
+
+	<div id="sub">
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h3>Boxxed</h3>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">			
+			<div class="col span_6_of_10">
+				<div class="paras_small">
+					<p>During it's life Boxxed has been a factory, a music venue, an urban gallery and now it's a business incubator that is opening it's doors to a weekend of coding.</p>
+                    <p class="left-align" >
+                        <strong>Address:</strong>
+                        <br/>
+                        104-108 Floodgate St, 
+                        <br/>
+                        Digbeth, Birmingham, 
+                        <br/>
+                        B5 5SR
+                    </p>
+				</div>
+			</div>		
+			<div class="col span_4_of_10">
+                <iframe width="100%" height="250px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=104-108+Floodgate+St,+Birmingham+B5+5SR&amp;aq=0&amp;oq=104-108+Floodgate+St,+B5+5SR&amp;sll=52.477749,-1.88364&amp;sspn=0.007084,0.021243&amp;ie=UTF8&amp;hq=&amp;hnear=104+Floodgate+St,+Birmingham+B5+5SR,+United+Kingdom&amp;t=m&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="https://maps.google.co.uk/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=104-108+Floodgate+St,+Birmingham+B5+5SR&amp;aq=0&amp;oq=104-108+Floodgate+St,+B5+5SR&amp;sll=52.477749,-1.88364&amp;sspn=0.007084,0.021243&amp;ie=UTF8&amp;hq=&amp;hnear=104+Floodgate+St,+Birmingham+B5+5SR,+United+Kingdom&amp;t=m&amp;z=14&amp;iwloc=A" style="color:#0000FF;text-align:left">View Larger Map</a></small>
 			</div>
 		</div>
 	</div>
