fa5e2caabd70c5a39cca1d98c49ad4e86c7e474a
Mike Elsmore <mikee@winningmoves.com>
2013-06-05 13:58:15 +0100

    Add the hackathon copy.

--- hackathon.php
+++ hackathon.php
@@ -8,11 +8,16 @@
 		<div class="section group">
 			<div class="col span_6_of_10">
 				<div class="paras">
-					<p>After a full day of talks from experts, time to test your metal </p>
+					<p>After a full day of talks comes a weekend packed with coding, testing your skills and seeing if you can build the best hack.</p>
+				</div>
+				<div class="paras_small">
+					<p>With some of the best <a href="<?php echo $site_url; ?>hackathon#partners" title="API Partners" >APIs and SDKs</a> in attendance and surrounded by developers and designers of every kind, something awesome can be built.</p>
+                    <p><span class="buy-button" ><a href="/buy" >Register Now</a></span></p>
+				</ul>
 				</div>
 			</div>
             <div class="col span_4_of_10" >
-                <img src="" />
+                <img src="<?php echo $site_url; ?>img/screamin_dev.jpg" />
             </div> 
 		</div>
 	</div>
@@ -20,15 +25,57 @@
 	<div id="sub">
 		<div class="section group">
 			<div class="col span_10_of_10 sub_heading">
-				<h3>Hackathon</h3>
+				<h3>Hack for success</h3>
 				<span class="line"></span>
 			</div>
 		</div>
 		<div class="section group">			
 			<div class="col span_10_of_10">
 				<div class="paras_small">
-					<p>After the conference a whole weekend of coding is afoot (August 31th and September 1st). That means 100 developers gathering together to build some great applications on different platforms in under 36 hours. With access to the knowledge gained at the conference and the APIs and SDKs of the hackathon providers, who will walk away with the prizes?</p>
+					<p>
+                        Go it alone or work as a team, in 27 Hours can you build something amazing and be the best hack?
+                        With challenges and prizes being set by Hackference and our <a href="<?php echo $site_url; ?>hackathon#partners" title="API Partners" >API Partners</a> there is plenty to aim for.
+                        At the end of the hack show of what you've built to the crowd and judges and try to take home the prize.
+                    </p>
 				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="sub">
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h3>Challanges and Prizes</h3>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">			
+			<div class="col span_10_of_10">
+				<div class="paras_small">
+					<p>The challanges and prizes will be announced closer to event.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="sub">
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+                <a name="partners" ></a>
+				<h3>API Partners</h3>
+				<span class="line"></span>
+			</div>
+		</div>
+
+		<div class="section group sponsors">
+			<div class="col span_1_of_4 sponsor">
+				<a href='http://www.soundcloud.com/'><img src='<?php echo $site_url; ?>img/sponsors/soundcloud.png' /></a>
+			</div>
+			<div class="col span_1_of_4 sponsor">
+				<a href='http://www.twilio.com/'><img src='<?php echo $site_url; ?>img/sponsors/twilio.png' /></a>
+			</div>
+			<div class="col span_1_of_4 sponsor">
+				<a href='http://joined.in/'><img src='<?php echo $site_url; ?>img/sponsors/joindin.png' /></a>
 			</div>
 		</div>
 	</div>
