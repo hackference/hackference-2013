71697061ad2b66182d6a909a629b7f5f197192c4
Mike Elsmore <mikee@winningmoves.com>
2013-06-21 09:35:25 +0100

    Add Terms for the Hackathon

---
+++ terms.php
@@ -0,0 +1,119 @@
+    <div id="sub">
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h2>Hackathon Terms of Attendance</h2>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10">
+                <div class="paras" >
+                    <p>Hackference Hackathon ("the event") is a creative 2 day building and making event taking place on August 31st / September 1st 2013 at Boxxed in Birmingham.</p>
+                </div>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h2>1. Binding Agreement</h2>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10">
+                <div class="paras_small" >
+                    <p>In order to attend Hackference Hackathon, you must agree to these Terms and Conditions ("Terms"). Please read these Terms carefully to ensure you understand and agree. These Terms form a binding legal agreement between you and The Organisers Mike Elsmore and Winning Moves Limited (“Winning Moves LTD”) with respect to the event.</p>
+                </div>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h2>2. Eligibility</h2>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10">
+                <div class="paras_small" >
+                    <p>This event is open to individuals who are at least 16 years of age or older on the date of the event. Anyone under the age of 16 on the date of the event must be accompanied by an adult at all times while at the event. To stay at the event overnight you must be at least 18 years of age or older on the date of the event. Anyone who is not staying overnight needs to vacate the premises by 8pm. All communications between the event and attendees will be in English, including but not limited to the website content and email communications.</p>
+                </div>
+			</div>
+        </div>
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h2>3. Conditions of Attendance</h2>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10">
+                <div class="paras_small" >
+                    <p>Attendees must have registered via the Hackference website or the dedicated event page on EventBrite, and also meet the eligibility requirements set forth above. By participating in the event, you agree to be bound by these Terms and to all decisions of Mike Elsmore and Winning Moves LTD, which are final, binding and conclusive in all matters. As a condition of participating in the event, you warrant to Mike Elsmore and Winning Moves LTD that any content you create:</p>
+                    <ul>
+                        <li>Does NOT include any unsuitable or offensive content, including nudity, sexually explicit, disparaging, libellous or other inappropriate content.</li>
+                        <li>Does NOT include any content that is in violation of or infringes third party intellectual property rights including, but not limited to copyrights, including music copyrights, trademarks, and rights of publicity.</li>
+                        <li>Is suitable for a general audience and does not contain any unsubstantiated, false and/or misleading claims.</li>
+                    </ul>
+                </div>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h2>4. License to Submissions</h2>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10">
+                <div class="paras_small" >
+                    <p>Each attendee will retain ownership of and all intellectual and industrial property rights to his or her creation. Mike Elsmore and Winning Moves LTD retains right to use individual creations; Each attendee grants Mike Elsmore and Winning Moves LTD UK a license to use individual creations only for the purposes of advertising and promotion of the event.</p>
+                </div>
+			</div>
+        </div>
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h2>5. Refusal of Attendance</h2>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10">
+                <div class="paras_small" >
+                    <p>An attendee may be prohibited from attending or removed from this event if, in Mike Elsmore and Winning Moves LTD’s sole discretion, it reasonably believes that the attendee has attempted to undermine the legitimate operation of the event by:</p>
+                    <ul>
+                        <li>Providing false information concerning his/her identity or contact details;</li>
+                        <li>Breaching or otherwise refusing to comply with any of the provisions set forth in these Terms;</li>
+                        <li>Threatening, harassing or interfering with the ability of other attendees to effectively participate in the event; </li>
+                        Or
+                        <li>Threatening, harassing or interfering with the event administrators, volunteers, partners or other employees.</li>
+                    </ul>
+                    <p>All decisions of Mike Elsmore and Winning Moves LTD in these matters are final and binding. Mike Elsmore and Winning Moves LTD reserves the right, in its sole discretion, to revoke any and all privileges associated with the event, and to take any other action it deems appropriate.</p>
+                </div>
+			</div>
+        </div>
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h2>6. Privacy</h2>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10">
+                <div class="paras_small" >
+                    <p>Attendees agree that any personal data entered during registration for the event or otherwise provided to Mike Elsmore and Winning Moves LTD in the course of participation in the event, including name, mailing address, phone number, birth date and email address, may be used by Mike Elsmore and Winning Moves LTD to verify attendees eligibility and contact attendees and otherwise will be processed, stored, and otherwise used for the purposes and within the context of the event.</p>
+                </div>
+			</div>
+        </div>
+		<div class="section group">
+			<div class="col span_10_of_10 sub_heading">
+				<h2>7. General; Miscellaneous</h2>
+				<span class="line"></span>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_10_of_10">
+                <div class="paras_small" >
+                    <p>Mike Elsmore and Winning Moves LTD reserves the right to limit the participation of any person in the event, amend or interpret these Terms or official communications governing the event or cancel the event for any reason with prior notice. Notices for any such amendment, interpretation or cancellation will be deemed given by posting on the event website and by virtue of a attendees continued participation in the event. These Terms will be governed by the laws of England and the English courts shall have exclusive jurisdiction.</p>
+                </div>
+			</div>
+        </div>
+	</div>
