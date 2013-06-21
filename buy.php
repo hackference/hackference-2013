4e19a5b09035341afdcc825dae7738fcc11f928b
Mike Elsmore <mikee@winningmoves.com>
2013-06-21 09:55:15 +0100

    Fix EventBrite ticekts iFrame and remove hackthon tickets notice

--- buy.php
+++ buy.php
@@ -8,11 +8,8 @@
 		<div class="section group">
 			<div class="col span_10_of_10">
 				<!-- EventBrite JS Block -->
-                <div style="width:100%; text-align:left;" ><iframe  src="https://www.eventbrite.co.uk/tickets-external?eid=5963331485&ref=etckt&v=2" frameborder="0" height="339" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:10px; padding:5px 0 5px; margin:2px; width:100%; text-align:left;" ><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.co.uk/r/etckt">Sell Tickets Online</a> <span style="color:#ddd;">through</span> <a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.co.uk?ref=etckt">Eventbrite</a></div></div>
+                <div style="width:100%; text-align:left;" ><iframe  src="https://www.eventbrite.co.uk/tickets-external?eid=5963331485&ref=etckt&v=2" frameborder="0" height="400" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:10px; padding:5px 0 5px; margin:2px; width:100%; text-align:left;" ><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.co.uk/r/etckt">Sell Tickets Online</a> <span style="color:#ddd;">through</span> <a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.co.uk?ref=etckt">Eventbrite</a></div></div>
 				<!-- EventBrite JS Block -->
-                <div class="paras" >
-                    <p>Hackathon tickets will be released shortly, just getting confirmation on venue details for overnight.</p>
-                </div>
 			</div>
 		</div>
 	</div>
