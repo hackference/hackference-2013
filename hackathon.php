7813ffde90c6d8605a1b1ffd61ed4cde5bf229e4
Mike Elsmore <mikee@winningmoves.com>
2013-07-03 14:02:02 +0100

    Add CouchBase as a participant at the hackathon

--- hackathon.php
+++ hackathon.php
@@ -96,6 +96,11 @@
 			</div>
 		</div>
 		<div class="section group sponsors">
+			<div class="col span_1_of_4 sponsor">
+				<a href='http://www.couchbase.com/' title="Couchbase"><img src='<?php echo $site_url; ?>img/sponsors/couchbase.png' /></a>
+			</div>
+		</div>
+		<div class="section group sponsors">
             <h4>A little help from our friends:</h4>
 			<div class="col span_1_of_4 sponsor">
 				<a href='http://www.sendgird.com/' title="SendGrid"><img src='<?php echo $site_url; ?>img/sponsors/sendgrid.png' /></a>
