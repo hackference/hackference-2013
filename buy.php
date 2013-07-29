adec7c110fbe0223930891dab4c7345cce7ceb92
Mike Elsmore <mikee@winningmoves.com>
2013-07-29 22:25:27 +0100

    Add extra BUY copy to the Buy page.

--- buy.php
+++ buy.php
@@ -7,9 +7,27 @@
 		</div>
 		<div class="section group">
 			<div class="col span_10_of_10">
+				<div class="paras_small">
+                    <p>As well as rubbing shoulders with the best developers and hackers, your tickets will also include:</p>
+                    <div class="section group">
+                        <div class="col span_2_of_10">
+                        &nbsp;
+                        </div>
+                        <div class="col span_6_of_10">
+                            <ul>
+                                <li>Food and drink</li>
+                                <li>Lanyards which are provided by <a href="http://www.soundcloud.com/" title="SoundCloud">SoundCloud</a></li>
+                                <li>Plenty of swag from sponsors</li>
+                            </ul>
+                        </div>
+                        <div class="col span_2_of_10">
+                        &nbsp;
+                        </div>
+                    </div>
+                </div>
 				<!-- EventBrite JS Block -->
                 <div style="width:100%; text-align:left;" >
-                    <iframe  src="https://www.eventbrite.co.uk/tickets-external?eid=5963331485&ref=etckt&v=2<?php if(isset($_REQUEST['discount'])){ echo'&discount=',$_REQUEST['discount']; } ?>" frameborder="0" height="550" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+                    <iframe  src="https://www.eventbrite.co.uk/tickets-external?eid=5963331485&ref=etckt&v=2<?php if(isset($_SESSION['discount'])){ echo'&discount=',$_SESSION['discount']; } ?>" frameborder="0" height="550" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
                     <div style="font-family:Helvetica, Arial; font-size:10px; padding:5px 0 5px; margin:2px; width:100%; text-align:left;" ><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.co.uk/r/etckt">Sell Tickets Online</a> <span style="color:#ddd;">through</span> <a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.co.uk?ref=etckt">Eventbrite</a></div>
                 </div>
 				<!-- EventBrite JS Block -->
