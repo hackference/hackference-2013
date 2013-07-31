bb4485d9ded3ae6518834142857822517394714b
Mike Elsmore <mikee@winningmoves.com>
2013-07-31 16:53:08 +0100

    Proudly got Pusher

--- hackathon.php
+++ hackathon.php
@@ -102,6 +102,9 @@
 			<div class="col span_1_of_4 sponsor">
 				<a href='http://developer.paypal.com/' title="PayPal|Developer" ><img src='<?php echo $site_url; ?>img/sponsors/paypal_developer.png' /></a>
 			</div>
+			<div class="col span_1_of_4 sponsor">
+				<a href='http://pusher.com/' title="Pusher" ><img src='<?php echo $site_url; ?>img/sponsors/pusher.png' /></a>
+			</div>
 		</div>
 		<div class="section group sponsors">
             <h4>A little help from our friends:</h4>
