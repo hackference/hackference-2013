a010a7f8cb2a25ea447650f96616f367daaa8544
Mike Elsmore <mikee@winningmoves.com>
2013-08-02 16:43:57 +0100

    Add Corona Labs as at the Hack

--- hackathon.php
+++ hackathon.php
@@ -105,6 +105,9 @@
 			<div class="col span_1_of_4 sponsor">
 				<a href='http://pusher.com/' title="Pusher" ><img src='<?php echo $site_url; ?>img/sponsors/pusher.png' /></a>
 			</div>
+			<div class="col span_1_of_4 sponsor">
+				<a href='http://www.coronalabs.com/' title="Corona Labs" ><img src='<?php echo $site_url; ?>img/sponsors/corona.png' /></a>
+			</div>
 		</div>
 		<div class="section group sponsors">
             <h4>A little help from our friends:</h4>
